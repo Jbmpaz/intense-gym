b5c9b18760a28fad9f85eebcb4d44bcec39e7da5
Jbmpaz <joelbatistamarciel@gmail.com>
2026-02-03 23:11:26 -0300

    fix: header e configurando o tailwind

---
+++ componetes/head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intense Gym</title>
+
+    <link rel="stylesheet" href="dist/styles.css">
+    <style>
+        .container {
+            max-width: 1244px;
+            width: 90%;
+            margin: auto;
+        }
+    </style>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+include "componetes/head.php";
+?>
+</head>
+<body>
+    <?php
+    include "componetes/header.php";
+    include "componetes/banner.php";
+    ?>
+</body>
+</html>
